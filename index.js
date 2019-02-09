5133c9302988f4406b31a705419dc500ef040152
ravindra rawat <ravinrawat.wd@gmail.com>
2019-02-09 16:40:59 -0700

    created store and contected with app

--- index.js
+++ index.js
@@ -2,9 +2,21 @@
  * @format
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
+import React from 'react';
+import { Provider } from 'react-redux';
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-natve'/;
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+import configureStore from './src/store/configureStore';
+
+const store = configureStore()
+
+const RNStore = ()=>(
+  <Provider>
+  <App/>
+  </Provider>
+)
+
+AppRegistry.registerComponent(appName, () => RNStore);
